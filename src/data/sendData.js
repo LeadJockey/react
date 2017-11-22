9469ba43f3e3690745de1701eb598f130f9d6253
LeadJockey <dltldnjs5790@naver.com>
2017-11-22 22:32:06 +0900

    thinking in react

---
+++ src/data/sendData.js
@@ -0,0 +1,6 @@
+/**
+ * Created by shawn on 2017. 11. 22..
+ */
+import mock from './mock.json';
+const list = mock.list;
+export default ((data) => data)(list);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ThinkingInReact from './components/thinkingInReact.js';
-
-import registerServiceWorker from './registerServiceWorker';
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<ThinkingInReact />, document.getElementById('root'));
-registerServiceWorker();
+import ThinkingInReact from './components/thinkingInReact';
+//import PRODUCTS from './data/mock.json';
+import FilterableProductTable from './components/FilterableProductTable';
+import data from './data/sendData';
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+ReactDOM.render(<FilterableProductTable products={data} />, document.getElementById('container'));
+//ReactDOM.render(<ThinkingInReact />, document.getElementById('root'));
